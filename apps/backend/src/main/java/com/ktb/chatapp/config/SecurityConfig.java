c210235484146431f3c7f7a1e6606473151119ba
Superson7 <vin990227@naver.com>
2025-12-11 17:12:35 +0900

    fix : CORS SecurityConfig for backend cicd test

--- apps/backend/src/main/java/com/ktb/chatapp/config/SecurityConfig.java
+++ apps/backend/src/main/java/com/ktb/chatapp/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     private final CustomBearerTokenResolver bearerTokenResolver;
     private final SessionAwareJwtAuthenticationConverter jwtAuthenticationConverter;
 
-    private static final List<String> CORS_ALLOWED_ORIGINS = List.of("http://localhost:3000/");
+    private static final List<String> CORS_ALLOWED_ORIGINS = List.of("https://chat.goorm-ktb-005.goorm.team");
 
     private static final List<String> CORS_ALLOWED_HEADERS = List.of(
             "Content-Type",
